677d44720be5277d2a3b7c5faae026e210ccf477
海礞 <32067229+haimengself@users.noreply.github.com>
2017-10-18 16:25:45 +0800

    Update jiecheng.cpp

--- jiecheng.cpp
+++ jiecheng.cpp
@@ -1,29 +1,29 @@
 #include<stdio.h>
-int factorial( int n );
+int factorial(int n);
 
-int main(  )
+int main()
 {
-	int n,res ;
+	int n,res;
 	
-	scanf( "%d",&n ) ;
-	res = factorial( n ) ;
+	scanf("%d",&n);
+	res = factorial(n);
 	
-	printf( "res = %d\n",res ) ;
-	return 0 ;
+	printf("res = %d\n",res);
+	return 0;
 }
 
-int factorial( int n )
+int factorial(int n)
 {
-	int i , t ;
-	i = 1 ;
-	t = 2 ;
+	int i,t;
+	i = 1;
+	t = 2;
 	
-	if(n <= 1) return 1 ;
+	if(n <= 1) return 1;
 	while(n-- > 1)
 	{
-		i = t * i ;
-		t = t + 1 ;
+		i = t * i;
+		t = t + 1;
 	}
 	
-	return i ;
+	return i;
 }
